c4a0dbbc70cc195d2863a1733ffe4b305f34583d
graytonio <g-buddy@comcast.net>
2018-12-04 10:02:58 -0500

    Added new server stuture protocol

--- LEDStripStringify/LEDStripStringify.ino
+++ LEDStripStringify/LEDStripStringify.ino
@@ -1,28 +1,71 @@
 #include <ArduinoJson.h>
 #include <ESP8266WiFi.h>
 #include <ESP8266HTTPClient.h>
-
+#include "creds.h"
+#include "FS.h"
 
 #define r 16
 #define g 5
 #define b 4
 
-const char* ssid = "2C";
-const char* password = "";
 HTTPClient http;
+String id;
  
 void setup () {
+  //Set lights to off durring boot
+  pinMode(r, OUTPUT);
+  pinMode(g, OUTPUT);
+  pinMode(b, OUTPUT);
+  setLight(0, 0, 0);
+  
+  SPIFFS.begin();
   Serial.begin(115200);
+
+  //Connect to Wifi Connection
   WiFi.begin(ssid, password);
   Serial.print("Connecting");
   while (WiFi.status() != WL_CONNECTED) {
     delay(1000);
     Serial.print(".");
   }
+  Serial.println("\nConnected Successfully");
+  //Resolve device ID
+  getID();
+}
 
-  pinMode(r, OUTPUT);
-  pinMode(g, OUTPUT);
-  pinMode(b, OUTPUT);
+void getID(){
+  if(SPIFFS.exists("/config")){
+    File idConfig = SPIFFS.open("/config", "r");
+    while(idConfig.available()) id += char(idConfig.read());
+    idConfig.close();
+    
+    http.begin(getPath + id);
+    int httpCode = http.GET();
+    http.end();
+    if(httpCode == 404) newDevice();
+  }else{
+    Serial.println("File does not exist");
+    newDevice();
+  }
+}
+
+void newDevice(){
+  Serial.println("Sending create request to server");
+  //Send request for new device
+  http.begin(newPath);
+  http.addHeader("Content-Type", "application/json");
+  int httpCode = http.POST(initArgs);
+
+  //Save response ID
+  DynamicJsonBuffer jsonBuffer(512);
+  JsonObject& root = jsonBuffer.parseObject(http.getString());
+  id = root["id"].as<String>();
+  http.end();
+
+  //Write to internal file system
+  File idConfig = SPIFFS.open("/config", "w");
+  idConfig.print(id);
+  idConfig.close();
 }
 
 void setLight(int r_in, int g_in, int b_in){
@@ -34,7 +77,7 @@ void setLight(int r_in, int g_in, int b_in){
 void loop() {
   DynamicJsonBuffer jsonBuffer(512);
   if (WiFi.status() == WL_CONNECTED) { 
-    http.begin("http://pers.conheart.com/led");
+    http.begin(getPath + id);
     int httpCode = http.GET();
     if (httpCode > 0) {
       JsonObject& root = jsonBuffer.parseObject(http.getString());
